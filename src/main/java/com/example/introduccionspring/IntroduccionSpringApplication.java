001905fc764179334e9f0d106763af5f1e930fc9
flaars <fcateloiu08@gmail.com>
2023-02-09 15:46:10 +0100

    Ya tengo la introduccion del Spring Boot

--- src/main/java/com/example/introduccionspring/IntroduccionSpringApplication.java
+++ src/main/java/com/example/introduccionspring/IntroduccionSpringApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntroduccionSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntroduccionSpringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(IntroduccionSpringApplication.class, args);
+    }
 }
